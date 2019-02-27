464f26b65a4c1ab09d6ba6e5a30830dd57b47928
Griffin Foster <griffin.foster@gmail.com>
2019-02-27 14:56:15 -0800

    added LCU command generator

--- scripts/generateLuMPScript.py
+++ scripts/generateLuMPScript.py
@@ -9,30 +9,31 @@ parser = argparse.ArgumentParser(
     description='''Generate a LuMP recorder command from a ARTEMIS3 YAML config file''')
 parser.add_argument('configFile', help='YAML config file')
 parser.add_argument('--templateDir', help='jinja2 template dir', default='../templates')
-parser.add_argument('-t', '--template', help='jinja2 template', default='LuMP_recorder.j2')
+parser.add_argument('-t', '--template', help='jinja2 LuMP template', default='LuMP_recorder.j2')
 parser.add_argument('-o', '--output', help='Output script name, default: YAML config name with _laneX.sh')
 parser.add_argument('-s', '--start_date', help='Override the config file start date, format: YYYY-MM-DDThh:mm:ssZ or none')
 parser.add_argument('-v', '--verbose', help='Verbose mode', action='store_true')
 args = parser.parse_args()
 
 configData = yaml.load(open(args.configFile))
+lumpDict = configData['LuMP']
 
-configData['generator_script'] = os.path.basename(__file__)
+lumpDict['generator_script'] = os.path.basename(__file__)
 
 # check for start_date
-if 'start_date' in configData:
-    configData['opt_arg'] = '--start_date=%s'%configData['start_date']
+if 'start_date' in lumpDict:
+    lumpDict['opt_arg'] = '--start_date=%s'%lumpDict['start_date']
 
 # override start_date
 if not(args.start_date is None):
-    if args.start_date.startswith('none'): configData['opt_arg'] = ''
-    else: configData['opt_arg'] = '--start_date=%s'%args.start_date
+    if args.start_date.startswith('none'): lumpDict['opt_arg'] = ''
+    else: lumpDict['opt_arg'] = '--start_date=%s'%args.start_date
 
 # build a config dict for each lane
-configBase = configData.copy()
+configBase = lumpDict.copy()
 configBase.pop('lane', None)
 
-for lane in configData['lane']:
+for lane in lumpDict['lane']:
     configLane = {**configBase, **lane}
 
     #Load Jinja2 template
