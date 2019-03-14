f421c3bcb9f8e4b8be31ba9ed7d8a188fc7378a0
Griffin Foster <griffin.foster@gmail.com>
2019-03-14 21:20:43 +0100

    restrutured the obs config and generator script

--- scripts/LCU/B1133+16_HBA_LCU.sh
+++ scripts/LCU/B1133+16_HBA_LCU.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# generated with generateLCUScript.py
+# generated with generateObsScripts.py on 2019-03-14 21:16:43.599194
 
 rspctl --bitmode=8
 
-/opt/lofar/bin/beamctl --antennaset=HBA_JOINED --rcus=0:191 --band=110_190 --beamlets=0:487 --subbands=12:499 --anadir=3.037109,0.276656,J2000 --digdir=3.037109,0.276656,J2000 &
+/opt/lofar/bin/beamctl --antennaset=HBA_JOINED --rcus=0:191 --band=110_190 --beamlets=0:487 --subbands=12:499 --anadir=3.037109.6,0.276656.6,J2000 --digdir=3.037109.6,0.276656.6,J2000 &
--- scripts/LuMP/B1133+16_HBA_lane4.sh
+++ scripts/LuMP/B1133+16_HBA_lane4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# generated with generateLuMPScript.py
+# generated with generateObsScripts.py on 2019-03-14 21:16:43.599194
 
 # lane 4
-/home/artemis/lump-lofar-und-mpifr-pulsare/lump/bin/Basic_LuMP_Recorder.py --port=UDP:16083 --clock_speed=200 --beamlets_per_lane=122 --datadir=/local_data/ARTEMIS/20190226_B1133+16 --data_type_in=L_intComplex16_t --station_name=UK608 --writer_type=LuMP1 --physical_beamlet_array=[366:488] --rcumode_array=[5]*122 --epoch_array=[J2000]*122 --verbose --duration=3600 --subband_array=[377:499] --filename_base=B1133+16_lane4 --sourcename_array=[B1133+16]*122 --rightascension_array=[3.037109]*122 --declination_array=[0.276656]*122 --recorder_num_cores=2 
+/home/artemis/lump/lump/bin/Basic_LuMP_Recorder.py --port=UDP:16083 --clock_speed=200 --beamlets_per_lane=122 --datadir=/local_data/ARTEMIS/20190314_211643_B1133+16 --data_type_in=L_intComplex16_t --station_name=UK608 --writer_type=LuMP1 --physical_beamlet_array=[366:488] --rcumode_array=[5]*122 --epoch_array=[J2000]*122 --verbose --duration=60 --subband_array=[377:499] --filename_base=B1133+16_lane4 --sourcename_array=[B1133+16]*122 --rightascension_array=[3.037109.6]*122 --declination_array=[0.276656.6]*122 --recorder_num_cores=2 
