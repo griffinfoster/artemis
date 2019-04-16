bb7f2da1e98f0f8748acf19042ceea2ff81356cd
artemis@adagio2 <artemis@adagio2>
2019-04-16 08:26:29 +0000

    lst.py reporting source elevation

--- scripts/lst.py
+++ scripts/lst.py
@@ -10,6 +10,7 @@ import argparse
 parser = argparse.ArgumentParser(
     description='''Print the UTC and LST for an observatory''')
 parser.add_argument('-o', '--observatory', help='Observatory name, default: UK608', default='UK608')
+parser.add_argument('-s', '--source', help='source name, default B051+21', default='PSR B0531+21')
 args = parser.parse_args()
 
 t = astropy.time.Time.now()
@@ -22,4 +23,6 @@ lst = t.sidereal_time('mean')
 print('Observatory:', args.observatory, t.location.to_geodetic())
 print('UTC: ', t)
 print('LST: ', lst)
-
+source = astropy.coordinates.SkyCoord.from_name(args.source)
+sourcealtaz = source.transform_to(astropy.coordinates.AltAz(obstime=t,location=t.location))
+print("Elevation = {0.alt:.2}".format(sourcealtaz))
